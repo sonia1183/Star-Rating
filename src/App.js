dbde0729dae97dc91c81676117e3ef6e2685b4e2
sonia1183 <80530004+sonia1183@users.noreply.github.com>
2021-10-14 10:00:45 +0530

    2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import './App.css';
-//import Div1 from './Div1';
-//import Div2 from './Div2';
-//import Display from './Display';
-//import Calculator from './calculator';
 import StarRating from './StarRating';
 function App() {
   return (
