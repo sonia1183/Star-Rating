21dee9b0c395d6118f554303698c86866482cd82
sonia1183 <80530004+sonia1183@users.noreply.github.com>
2021-10-14 09:59:27 +0530

    Delete Div1.js

--- src/Div1.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function Div1(props){
-    return(
-        <div style={{backgroundColor : props.color,width:"200px",height:"200px"}}>{props.color}</div>
-    );
-}
-export default Div1;
